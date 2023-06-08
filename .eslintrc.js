615e4cd1333c0aaee3e508d4da58b6a60b5a86a9
alichatraei <avida.chatraee2001@yahoo.com>
2023-06-08 05:51:09 +0330

    fix: fix husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'prettier',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "prettier",
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh', 'prettier'],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["react-refresh", "prettier"],
   rules: {
-    'react-refresh/only-export-components': 'warn',
-    'no-undef': 'off',
+    "react-refresh/only-export-components": "warn",
+    "no-undef": "off",
   },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from "react"
-import { useState } from "react"
 import "./App.css"
 
 function App() {
-  const [state] = useState()
   return (
     <React.Fragment>
       <h1></h1>
