7812116ca155a098b4c06d849227ed6017504cd3
alichatraei <avida.chatraee2001@yahoo.com>
2023-06-08 08:36:24 +0330

    feat: add backend folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react-refresh", "prettier"],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh', 'prettier'],
   rules: {
-    "react-refresh/only-export-components": "error",
-    unused: "error",
-    "no-undef": "off",
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'error',
+    'no-undef': 'off',
   },
 }
