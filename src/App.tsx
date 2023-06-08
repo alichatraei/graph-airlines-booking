10f7a49a2d56cbc9dfab16f9968a3b1461dbbffa
alichatraei <avida.chatraee2001@yahoo.com>
2023-06-08 05:30:48 +0330

    feat: change package json for husky

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 import "./App.css"
 
 function App() {
-  const [state] = useState()
+  const [state, setState] = useState()
   return (
     <React.Fragment>
       <h1></h1>
